a076cd9f401227ff7cc0d64b20b4b6cf87da6215
danekkkk <101827298+danekkkk@users.noreply.github.com>
2023-12-10 13:31:32 +0100

    commit

---
+++ Grid-ex1/Grid-ex1/Grid-ex1/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace Grid_ex1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
